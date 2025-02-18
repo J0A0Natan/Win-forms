b96ecd6432ecb41aa7d83ad9884fcc6d754826de
J0A0Natan <joao@prymelab.com.br>
2025-02-18 17:42:52 -0300

    Adicionado opção de DB ao abrir aplicação, ainda não é possivel configurar

--- ProjectOne/ProjectOne/ConfigDB.vb
+++ ProjectOne/ProjectOne/ConfigDB.vb
@@ -1,3 +1,19 @@
 ﻿Public Class ConfigDB
+    Private Sub BtnSelecionar_Click(sender As Object, e As EventArgs) Handles BtnSelecionar.Click
+        Dim escolha As Integer
+        If RadioButton1.Checked = True Then
+            escolha = 1
+        ElseIf RadioButton2.Checked = True Then
+            escolha = 2
+        End If
 
+        'MessageBox.Show("Opção selecionada: " & escolha)
+        Form1.tipoConFormNum = escolha
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub ConfigDB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RadioButton1.Checked = True 
+    End Sub
 End Class
