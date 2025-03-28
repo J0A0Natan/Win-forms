b7ce612998caec052fa204bd16d3e6fdd16a6446
J0A0Natan <joao@prymelab.com.br>
2025-03-28 10:12:08 -0300

    Comentarios removidos

--- ProjectOne/ProjectOne/Relatorio.vb
+++ ProjectOne/ProjectOne/Relatorio.vb
@@ -4,23 +4,9 @@
     End Sub
 
     Private Sub Relatorio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        If Form1.tipoConFormNum = 1
+        If Form1.tipoConNum = 1 Then
             Dim cont = New ContatoAccsses
-            'Dim dt As DataTable
-            'Dim ds As DataSet1
-            'Dim rpt As New CrystalReport
-
-            'ds = cont.RelatorioCrystal()
-            'dt = ds.Tables(0)
-            'rpt.SetDataSource(dt)
-
             CrystalReportViewer1.ReportSource = cont.RelatorioCrystal()
-            
-            'For Each row As DataRow In dt.Rows
-            '    Console.WriteLine("ID: " & row("id").ToString)
-            '    Console.WriteLine("Nome: " & row("nome").ToString)
-            '    Console.WriteLine("E-mail: " & row("email").ToString)
-            'Next
         Else
             Dim cont = New ContatoSQLServer
             CrystalReportViewer1.ReportSource = cont.RelatorioCrystal()
