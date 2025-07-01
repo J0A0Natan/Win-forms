a46b9303eef4bcea9f59b78372f6e72e8543703b
J0A0Natan <joao@prymelab.com.br>
2025-07-01 08:27:23 -0300

    Apagado classes ContatoSQLServer e ContatoAccsses

--- ProjectOne/ProjectOne/ContatoAccsses.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports ApplicationBlocks.Data
-
-Public Class ContatoAccsses : Inherits Contato
-    Sub New()
-        Me.DbHelper = New OleDBDbHelper
-        Dim pathAcsses As String = My.Settings.CaminhoAcsses
-        Me.strCon = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=" & pathAcsses
-    End Sub
-End Class
